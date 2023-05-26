41137447327d3f64e85ff1d0a376fe4053984523
Guardado <ga18097@ues.edu.sv>
2023-05-25 18:41:46 -0600

    Tablas Producto, UnidadDeMedida y PrecioUnidadDeMedida
    Se crearon las tablas Producto, UnidadDeMedida y PrecioUnidadDeMedida. Sus Modelos y controladores con sus métodos. También se crearon los archivos de seeders y factories para estas tablas. Se definieron las rutas en el archivo api.php

---
+++ app/Http/Controllers/UnidadDeMedidaController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UnidadDeMedida;
+use Illuminate\Http\Request;
+
+class UnidadDeMedidaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Se retorna la lista de unidades de medida
+        return UnidadDeMedida::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Se definen las reglas de validación
+        $rules = [
+            'nombre' => 'required|string|max:60'
+        ];
+        $validador = \Validator::make($request->all(), $rules);
+        // Se valida que la variable $validador no haya fallado
+        if ($validador->fails()){
+            return response()->json([
+                'respuesta' => false,
+                'mensaje' => $validador->errors()->all()
+            ], 400);
+        }
+        // Se crea la unidad de medida con los datos ingresados
+        $unidadDeMedida = UnidadDeMedida::create($request->all());
+        // Se valida que la unidad de medida se haya creado correctamente
+        if (isset($unidadDeMedida)){
+            return response()->json([
+                'respuesta' => true,
+                'mensaje' => 'Unidad de medida creada correctamente',
+            ], 201);
+        }
+        // Si la unidad de medida no se creó correctamente, se retorna un mensaje de error
+        else{
+            return response()->json([
+                'respuesta' => false,
+                'mensaje' => 'Error al crear la unidad de medida',
+            ], 400);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UnidadDeMedida $id_unidad_de_medida)
+    {
+        // Se busca la unidad de medida por su id
+        $unidadDeMedida = UnidadDeMedida::find($id_unidad_de_medida);
+        // Se valida que la unidad de medida exista
+        if (isset($unidadDeMedida)){
+            // Se retorna la unidad de medida encontrada
+            return response()->json([
+                'respuesta' => true,
+                'mensaje' => 'Unidad de medida encontrada',
+                'unidadDeMedida' => $unidadDeMedida
+            ], 200);
+        }
+        // Si la unidad de medida no existe, se retorna un mensaje de error
+        else{
+            return response()->json([
+                'respuesta' => false,
+                'mensaje' => 'Unidad de medida no encontrada',
+            ], 400);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UnidadDeMedida $unidadDeMedida)
+    {
+        // Se definen las reglas de validación, igual que en el método store
+        $rules = [
+            'nombre' => 'required|string|max:60'
+        ];
+        // Se crea una variable $validador para almacenar el resultado de la validación
+        $validador = \Validator::make($request->all(), $rules);
+        // Se valida que la variable $validador no haya fallado
+        if ($validador->fails()){
+            return response()->json([
+                'respuesta' => false,
+                'mensaje' => $validador->errors()->all()
+            ], 400);
+        }
+        // Se actualiza la unidad de medida con los datos ingresados
+        $unidadDeMedida->update($request->all());
+        // Se valida que la unidad de medida se haya actualizado correctamente
+        if (isset($unidadDeMedida)){
+            return response()->json([
+                'respuesta' => true,
+                'mensaje' => 'Unidad de medida actualizada correctamente',
+            ], 201);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UnidadDeMedida $unidadDeMedida)
+    {
+        // Se elimina la unidad de medida
+        $unidadDeMedida->delete();
+        return response()->json([
+            'respuesta' => true,
+            'mensaje' => 'Unidad de medida eliminada correctamente',
+        ], 200);
+    }
+}
---
+++ database/migrations/2023_05_25_163120_modulo_inventario_crear_tabla_producto.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Producto', function (Blueprint $table) {
+            $table->string('codigo_barra_producto', 10)->primary();
+            $table->string('nombre_producto', 50);
+            $table->integer('cantidad_producto_disponible');
+            $table->decimal('precio_unitario');
+            $table->boolean('esta_disponible');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
