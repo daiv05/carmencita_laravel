7b859fd2da81598666a65905df0a29337cdec6b1
juanBM-1122 <100696845+juanBM-1122@users.noreply.github.com>
2023-08-26 15:04:59 -0600

    Control Lote 1.5v

--- app/Http/Controllers/LoteController.php
+++ app/Http/Controllers/LoteController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\Lote;
 use Illuminate\Support\Facades\DB;
 use App\Http\Resources\PaginacionLoteRecursos;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
 
 class LoteController extends Controller
 {
@@ -16,4 +19,39 @@ class LoteController extends Controller
         //return Lote::with("producto")->paginate(15);
         return PaginacioLoteRecurso::collection(Lote::paginate(10));
     }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+
+        ]);
+    }
+
+    public function show(){
+        
+    }
+
+    public function update(Request $request){
+        $validator = Validator::make($request->all(),[
+            "id_lote"=>Rule::exists("lotes","id_lote"),
+            "fecha_vencimiento"=>"date_format:Y-m-d|required",
+            "codigo_barra_producto"=>["required",Rule::exists("producto","codigo_barra_producto")],
+            "cantidad_total_unidades"=>"required|decimal:2",
+            "cantidad"=>"required|integer",
+            "precio_unitario"=>"required|decimal:2|integer",
+            "costo_total"=>"required|decimal:2"
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                "status"=>false,
+                "errores"=>$validator->messages(),
+            ],404);
+        }
+        $lote = Lote::where("id_lote",$request->input("id_lote"))->first();
+        $lote->fecha_vencimiento = $request->input("fecha_vencimiento");
+        $lote->precio_unitario = $request->input("precio_unitario");
+        $lote->costo_total = $request->input("costo_total");
+        $loye->codigo_barra_producto = $request->input("");
+
+    }
 }
+
