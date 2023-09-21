c454ee999df684d2c67fe816fa8372ff7cbeff48
daiv05 <dc19019@ues.edu.sv>
2023-09-21 16:01:47 -0600

    unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,14 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : null;
+        return $request->expectsJson() ? null : route('login');
+    }
+
+    protected function unauthenticated($request, array $guards)
+    {
+        abort(response()->json(
+            [
+                'is_auth' => false,
+            ], 401));
     }
 }
