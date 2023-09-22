6e84613f388ff71b8043a8a17ae792a609957899
daiv05 <dc19019@ues.edu.sv>
2023-09-22 15:56:13 -0600

    ordenar api v.10

--- routes/api.php
+++ routes/api.php
@@ -39,50 +39,26 @@ use App\Http\Controllers\PromocionesController;
 use App\Http\Controllers\VentaDomicilioController;
 use Illuminate\Console\View\Components\Info;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-
 
 /*Aquí solo registrar rutas a las que pueda acceder el gerente*/
 Route::middleware(['auth:sanctum', 'permission:all'])->group(function () {
     Route::get("empleados", [EmpleadoController::class, 'listaEmpleados']);
     Route::get('pacientes', [JornadaLaboralDiariaController::class, 'index']);
-    //Route::get('cargos',[CargoController::class,'index']);
-    //Route::get('cargos/{id_cargo}',[CargoController::class,'show']);
     Route::get('sexos', [SexoController::class, 'index']);
     Route::get('estado_familiar', [EstadoFamiliarController::class, 'index']);
     Route::get('nacionalidades', [NacionalidadController::class, 'index']);
     Route::post('empleado', [EmpleadoController::class, 'store']);
-
     Route::get('empleado/{empleado}', [EmpleadoController::class, 'show']);
-
     Route::put('empleado_update/{empleado}', [EmpleadoController::class, 'update']);
-
     Route::put('empleado_activo/{empleado}', [EmpleadoController::class, 'updateEstado']);
-
-
-
     //Rutas para cargos
     Route::resource('cargos', CargoController::class);
-
     //Ruta para paginacion
     Route::get('productos/paginacion/{cantidad_productos}', [ProductoController::class, 'getPaginacionProductos']);
-
     //Ruta para actualizar estado de producto
     Route::put('productos/updateEstado/{producto}', [ProductoController::class, 'updateEstado']);
-
     //Rutas para unidades de medida
     Route::resource('unidades_de_medida', UnidadDeMedidaController::class);
-
     //Rutas para precios de unidades de medida
     Route::resource('precios_unidades_de_medida', PrecioUnidadDeMedidaController::class);
     Route::post('precios_lista_unidades_de_medida', [PrecioUnidadDeMedidaController::class, "storeList"]);
@@ -91,17 +67,13 @@ Route::middleware(['auth:sanctum', 'permission:all'])->group(function () {
     Route::put('precio_lista_unidades/{codigo_de_barra}', [PrecioUnidadDeMedidaController::class, "updateList"]);
     //Rutas para jornadas laborales diarias
     Route::resource('jornadas_laborales_diarias', JornadaLaboralDiariaController::class);
-
     //Rutas para cargos
     Route::resource('cargos', CargoController::class);
-
-
     //Rutas para Cliente
     Route::resource('clientes', ClienteController::class);
 });
 
-Route::get('impresion_prueba', [ImpresionController::class, 'generatePDF']);
-
+// Route::get('impresion_prueba', [ImpresionController::class, 'generatePDF']);
 Route::get('pacientes',[JornadaLaboralDiariaController::class,'index']);
 
 /*Aqui poner las rutas para el cajero */ 
@@ -134,7 +106,6 @@ Route::middleware(["auth:sanctum","permission:all|Recursos Humanos"])->group(fun
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [LoginController::class, 'logout']);
-
     //Para marcar y obtener las asistencias del usuario logueado, todos los empleados lo pueden hacer
     Route::post('asistencia',[AsistenciaController::class,'store']);
     Route::get('asistencia', [AsistenciaController::class,'getAsistenciasEmpleado']);
@@ -143,37 +114,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
 Route::post("login", [LoginController::class, "authorization"]);
 
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
-
-
-//Route::post('cargos',[CargoController::class,'store']);
-
-
-
 
 //Rutas para DetalleVenta
 Route::resource('detalle_ventas', DetalleVentaController::class);
-
 //Rutas para Venta
 Route::resource('ventas', VentaController::class);
-
 //Rutas para CreditoFiscal
 Route::resource('credito_fiscals', CreditoFiscalController::class);
-
 //Rutas para DetalleCreditoFiscal
 Route::resource('detalle_creditos', DetalleCreditoController::class);
-
 //Ruta para buscar Producto por Nombre
 Route::get('productos/buscar/{nombre_producto}', [ProductoController::class, 'getProductoPorNombre']);
-
 //Ruta para obtener todos los nombres de los productos
-
 Route::get('productos/nombres/lista', [ProductoController::class, 'getNombresProductos']);
-
-Route::get('productos/nombres/lista', [ProductoController::class, 'getNombresProductos']);
-
 //Ruta para obtener las ventas y listarlas
 Route::get('ventasCF', [VentasCFController::class, 'index']);
 //Ruta para buscar una venta especifica
@@ -184,40 +137,27 @@ Route::delete('ventasCF/{id_venta}', [VentasCFController::class, 'eliminarVentaC
 Route::get('ventasCF_detalle/{id_venta}', [VentasCFController::class, 'obtenerVentaAndDetalle']);
 //Ruta para obtener un producto con sus precio de unidad de medida
 Route::get('productos/precios/{nombre_producto}', [ProductoController::class, 'getProductoConUnidadMedida']);
-
 //Ruta para obtener todos los identificadores de los clientes
 Route::get('clientes/identificador/lista', [ClienteController::class, 'getListaClientesIdentificadores']);
-
 //Rutas para Municipio
 Route::resource('municipios', MunicipioController::class);
-
 //Rutas para Departamento
 Route::resource('departamentos', DepartamentoController::class);
-
 //Ruta para obtener el departamento segun el nombre
 Route::get('departamentos/buscar/{nombre_departamento}', [DepartamentoController::class, 'getDepartamentoPorNombre']);
-
 Route::get('departamentos/buscar/{nombre_departamento}', [DepartamentoController::class, 'getDepartamentoPorNombre']);
-
-
 //Ruta para registrar una Venta con DetalleVenta junto
 Route::post('ventas/registrar', [VentaController::class, 'register_venta_detalle']);
-
 //Ruta para registrar un Credito con DetalleCredito junto
 Route::post('creditos/registrar', [CreditoFiscalController::class, 'register_credito_detalle']);
-
 //Para obtener los creditos fiscales
 Route::get('creditos', [VentasCFController::class, 'indexCF']);
-
 //Para buscar un credito fiscal especifico	
 Route::post('creditos/buscar', [VentasCFController::class, 'buscarCreditoF']);
-
 //Ruta para obtene un credito fiscal especifico y sus detalles
 Route::get('creditos_detalle/{id_credito}', [VentasCFController::class, 'obtenerCreditoAndDetalle']);
-
 //Ruta para actualizar estado de una venta
 Route::put('ventaCF/updateEstado/{ventaCF}', [VentasCFController::class, 'updateEstado']);
-
 //Ruta para actualizar estado de credito fiscal
 Route::put('creditos/updateEstado/{CFSales}',[VentasCFController::class,'updateEstadoCredito']);
 
@@ -228,6 +168,7 @@ Route::controller(HojaDeRutaController::class)->group(function () {
     Route::post('/hoja_de_ruta', 'store');
     Route::post('/hoja_de_ruta/marcar_entregada/{id}', 'marcarEntregada');
 });
+
 Route::get('/hoja_de_ruta_paginadas',[HojaDeRutaController::class,'obtenerHojasDeRutasPaginadasFiltro']);
 Route::post('/facturas_domicilio',[VentaController::class,'getVentasDomicilio']);
 Route::post('/creditos_fiscales_domicilio',[CreditoFiscalController::class,'getCreditosFiscalesDomicilio']);
@@ -250,19 +191,17 @@ Route::put('modificar_pedido_factura/{venta}',[VentaController::class,'update'])
 Route::put('modificar_pedido_credito/{creditoFiscal}',[CreditoFiscalController::class,'update']);
 Route::post('delete_pedido/credito_fiscal/{creditoFiscal}',[CreditoFiscalController::class,'destroy']);
 Route::post('delete_pedido/factura/{venta}',[VentaController::class,'destroy']);
+
 //Asistencia y Planillas
 Route::controller(HojaAsistenciaController::class)->group(function (){
     Route::post('hoja_asistencia','store');
 });
 
-/*Route::controller(AsistenciaController::class)->group(function (){
-    Route::post('asistencia','store');
-    Route::get('asistencia', 'getAsistenciasEmpleado');
-});*/
 
 Route::controller(PlanillaController::class)->group(function (){
     Route::post('planilla','store');
 });
+
 Route::put('creditos/updateEstado/{CFSales}', [VentasCFController::class, 'updateEstadoCredito']);
 
 Route::controller(VentaDomicilioController::class)->group(function () {
@@ -281,7 +220,6 @@ Route::controller(CreditoFiscalDomicilioController::class)->group(function () {
 
 //Para obtener los productos que vencen en los proximos 15 dias
 Route::get('productosXVenecer', [InformeProductosPorVencerController::class, 'index']);
-
 //Para creditos proveedores
 Route::apiResource('creditosProveedores', CreditoController::class);
 //para obtene los proveedores
