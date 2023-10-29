9019dd21d95a3b22c0fcb4a989dcfd50e3cb6fee
David DV <71719478+daiv05@users.noreply.github.com>
2023-10-28 22:36:09 -0600

    quitar impresion para el server

--- app/Http/Controllers/ImpresionController.php
+++ app/Http/Controllers/ImpresionController.php
@@ -15,87 +15,89 @@ class ImpresionController extends Controller
 {
     public function generate_pdf_consumidor_final(Venta $venta)
     {
-        error_log('imprimiendo desde detalle hr');
-        error_log($venta);
-        if ($venta == null) {
-            return response()->json([
-                'message' => 'No se encontró la venta'
-            ], 404);
-        }
-        $venta->with('detalleVenta');
-        foreach ($venta->detalleVenta as $detalle) {
-            $detalle->with('producto');
-            $cantidad = $detalle->cantidad_producto;
-            // Ordenar $detalle->producto->precioUnidadDeMedida en forma ascendente
-            $precios = $detalle->producto->precioUnidadDeMedida->sortBy('cantidad_producto');
-            // Obtener el primero
-            $precio_cercano = $precios->first();
-            if ($precios->isEmpty()) {
-                break;
-            }
-            if ($cantidad < $precio_cercano->cantidad_producto) {
-                $detalle->producto->precio_unitario = $detalle->producto->precio_unitario;
-                error_log('unitario actual');
-                error_log($detalle->producto->precio_unitario);
-                break;
-            } else {
-                foreach ($precios as $precio_ordenado) {
-                    if ($precio_ordenado->cantidad_producto <= $cantidad) {
-                        $precio_cercano = $precio_ordenado;
-                    } else {
-                        break;
-                    }
-                }
-                // Calcular el precio_producto basado en el promedio entre cantidad_producto y precio_unidad
-                $detalle->producto->precio_unitario = number_format($precio_cercano->precio_unidad_medida_producto / $precio_cercano->cantidad_producto, 4);
-            }
-        }
-        $pdf = Pdf::loadView('impresion_facturas', compact('venta'));
-        $ruta_pdf = 'factura_venta_' . $venta->id_venta . '.pdf';
-        $pdf->save($ruta_pdf);
-        $command1 = 'PDFXEdit /importp "C:\setting_3060.dat" ';
-        exec($command1, $output, $return_var);
-        $command2 = 'PDFXEdit /print:default=no;showui=no;printer="Canon G3060 series" ' . public_path($ruta_pdf);
-        exec($command2, $output1, $return_var1);
-        return implode(',', $output) . ' ' . implode(',', $output1);
+        return 'ok';
+        // error_log('imprimiendo desde detalle hr');
+        // error_log($venta);
+        // if ($venta == null) {
+        //     return response()->json([
+        //         'message' => 'No se encontró la venta'
+        //     ], 404);
+        // }
+        // $venta->with('detalleVenta');
+        // foreach ($venta->detalleVenta as $detalle) {
+        //     $detalle->with('producto');
+        //     $cantidad = $detalle->cantidad_producto;
+        //     // Ordenar $detalle->producto->precioUnidadDeMedida en forma ascendente
+        //     $precios = $detalle->producto->precioUnidadDeMedida->sortBy('cantidad_producto');
+        //     // Obtener el primero
+        //     $precio_cercano = $precios->first();
+        //     if ($precios->isEmpty()) {
+        //         break;
+        //     }
+        //     if ($cantidad < $precio_cercano->cantidad_producto) {
+        //         $detalle->producto->precio_unitario = $detalle->producto->precio_unitario;
+        //         error_log('unitario actual');
+        //         error_log($detalle->producto->precio_unitario);
+        //         break;
+        //     } else {
+        //         foreach ($precios as $precio_ordenado) {
+        //             if ($precio_ordenado->cantidad_producto <= $cantidad) {
+        //                 $precio_cercano = $precio_ordenado;
+        //             } else {
+        //                 break;
+        //             }
+        //         }
+        //         // Calcular el precio_producto basado en el promedio entre cantidad_producto y precio_unidad
+        //         $detalle->producto->precio_unitario = number_format($precio_cercano->precio_unidad_medida_producto / $precio_cercano->cantidad_producto, 4);
+        //     }
+        // }
+        // $pdf = Pdf::loadView('impresion_facturas', compact('venta'));
+        // $ruta_pdf = 'factura_venta_' . $venta->id_venta . '.pdf';
+        // $pdf->save($ruta_pdf);
+        // $command1 = 'PDFXEdit /importp "C:\setting_3060.dat" ';
+        // exec($command1, $output, $return_var);
+        // $command2 = 'PDFXEdit /print:default=no;showui=no;printer="Canon G3060 series" ' . public_path($ruta_pdf);
+        // exec($command2, $output1, $return_var1);
+        // return implode(',', $output) . ' ' . implode(',', $output1);
     }
 
     public function generate_pdf_credito_fiscal(CreditoFiscal $credito)
     {
-        $credito->with('detalleCredito')->with('cliente')->with('municipio')->with('departamento');
-        foreach ($credito->detalleCredito as $detalle) {
-            $detalle->with('producto');
-            $cantidad = $detalle->cantidad_producto_credito;
-            // Ordenar $detalle->producto->precioUnidadDeMedida en forma ascendente
-            $precios = $detalle->producto->precioUnidadDeMedida->sortBy('cantidad_producto');
-            if ($precios->isEmpty()) {
-                break;
-            }
-            // Obtener el primero
-            $precio_cercano = $precios->first();
-            if ($cantidad < $precio_cercano->cantidad_producto) {
-                $detalle->producto->precio_unitario = $detalle->producto->precio_unitario;
-                break;
-            } else {
-                foreach ($precios as $precio_ordenado) {
-                    if ($precio_ordenado->cantidad <= $cantidad) {
-                        $precio_cercano = $precio_ordenado;
-                    } else {
-                        break;
-                    }
-                }
-                // Calcular el precio_producto basado en el promedio entre cantidad_producto y precio_unidad
-                $detalle->producto->precio_unitario = number_format($precio_cercano->precio_unidad_medida_producto / $precio_cercano->cantidad_producto, 4);
-            }
-        }
-        $pdf = Pdf::loadView('impresion_creditos', compact('credito'));
-        $ruta_pdf = 'factura_credito_' . $credito->id_creditofiscal . '.pdf';
-        $pdf->save($ruta_pdf);
-        $command1 = 'PDFXEdit /importp "C:\setting_3060.dat" ';
-        exec($command1, $output, $return_var);
-        $command2 = 'PDFXEdit /print:default=no;showui=no;printer="Canon G3060 series" ' . public_path($ruta_pdf);
-        exec($command2, $output1, $return_var1);
-        return implode(',', $output) . ' ' . implode(',', $output1);
+        return 'ok';
+        // $credito->with('detalleCredito')->with('cliente')->with('municipio')->with('departamento');
+        // foreach ($credito->detalleCredito as $detalle) {
+        //     $detalle->with('producto');
+        //     $cantidad = $detalle->cantidad_producto_credito;
+        //     // Ordenar $detalle->producto->precioUnidadDeMedida en forma ascendente
+        //     $precios = $detalle->producto->precioUnidadDeMedida->sortBy('cantidad_producto');
+        //     if ($precios->isEmpty()) {
+        //         break;
+        //     }
+        //     // Obtener el primero
+        //     $precio_cercano = $precios->first();
+        //     if ($cantidad < $precio_cercano->cantidad_producto) {
+        //         $detalle->producto->precio_unitario = $detalle->producto->precio_unitario;
+        //         break;
+        //     } else {
+        //         foreach ($precios as $precio_ordenado) {
+        //             if ($precio_ordenado->cantidad <= $cantidad) {
+        //                 $precio_cercano = $precio_ordenado;
+        //             } else {
+        //                 break;
+        //             }
+        //         }
+        //         // Calcular el precio_producto basado en el promedio entre cantidad_producto y precio_unidad
+        //         $detalle->producto->precio_unitario = number_format($precio_cercano->precio_unidad_medida_producto / $precio_cercano->cantidad_producto, 4);
+        //     }
+        // }
+        // $pdf = Pdf::loadView('impresion_creditos', compact('credito'));
+        // $ruta_pdf = 'factura_credito_' . $credito->id_creditofiscal . '.pdf';
+        // $pdf->save($ruta_pdf);
+        // $command1 = 'PDFXEdit /importp "C:\setting_3060.dat" ';
+        // exec($command1, $output, $return_var);
+        // $command2 = 'PDFXEdit /print:default=no;showui=no;printer="Canon G3060 series" ' . public_path($ruta_pdf);
+        // exec($command2, $output1, $return_var1);
+        // return implode(',', $output) . ' ' . implode(',', $output1);
     }
 
     public function ver_factura(Request $request)
@@ -230,22 +232,24 @@ class ImpresionController extends Controller
 
     public function generate_pdf_hoja_de_ruta($id_hr)
     {
-        $hoja_de_ruta = HojaDeRuta::find($id_hr);
-        
-        $hoja_de_ruta->empleado;
-        
-        foreach ($hoja_de_ruta->creditoFiscalDomicilio as $credito) {
-            $credito->creditoFiscal;
-            $credito->creditoFiscal->cliente;
-        }
+        return "ok";
 
-        foreach ($hoja_de_ruta->ventaDomicilio as $venta) {
-            $venta->venta;
-        }
+        // $hoja_de_ruta = HojaDeRuta::find($id_hr);
         
-        $pdf = Pdf::loadView('impresion_hoja_de_ruta', compact('hoja_de_ruta'));
-        $ruta_pdf = 'resumen_hoja_' . $hoja_de_ruta->id_hr . '.pdf';
-        $pdf->save($ruta_pdf);
+        // $hoja_de_ruta->empleado;
+        
+        // foreach ($hoja_de_ruta->creditoFiscalDomicilio as $credito) {
+        //     $credito->creditoFiscal;
+        //     $credito->creditoFiscal->cliente;
+        // }
+
+        // foreach ($hoja_de_ruta->ventaDomicilio as $venta) {
+        //     $venta->venta;
+        // }
+        
+        // $pdf = Pdf::loadView('impresion_hoja_de_ruta', compact('hoja_de_ruta'));
+        // $ruta_pdf = 'resumen_hoja_' . $hoja_de_ruta->id_hr . '.pdf';
+        // $pdf->save($ruta_pdf);
 
         /*$command1 = 'PDFXEdit /importp "C:\setting_3060.dat" ';
         exec($command1, $output, $return_var);
